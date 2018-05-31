1fe2ce4f38bbb4779da725e1f4cb76ae6ee68eba
stefan9x <stefanj94@live.com>
2018-05-31 16:14:16 +0200

    Small changes

--- CryptoLib/jni/crypto.cpp
+++ CryptoLib/jni/crypto.cpp
@@ -7,22 +7,24 @@
 JNIEXPORT jstring JNICALL Java_stefan_jovanovic_chatapplication_Crypto_crypt (JNIEnv *env, jobject obj, jstring message)
 {
     const char *msg = env->GetStringUTFChars(message, NULL);
-    char key[16] = "This is the key";
+    char *key;
+    key = "This is the key";
     int size = strlen(msg);
-    char* crypted = (char*)malloc(size * sizeof(char));
+    char* crypted = (char*)malloc(size*sizeof(char));
+    memset(crypted, 0, size);
 
     int i=0;
     int key_cnt = 0;
-    for (i = 0; i<strlen(msg); i++)
+    for (i = 0; i<size; i++)
     {
-        if (key[key_cnt] != msg[i]) {
+        if (key[key_cnt] != msg[i]){
             char temp = key[key_cnt] ^ msg[i];
             crypted[i] = temp;
             key_cnt++;
             if (key_cnt == 16) key_cnt = 0;
-        }
-        else {
-            crypted[i] = msg[i];
+        } else {
+            char temp = msg[i];
+            crypted[i] = temp;
             key_cnt++;
             if (key_cnt == 16) key_cnt = 0;
         }
